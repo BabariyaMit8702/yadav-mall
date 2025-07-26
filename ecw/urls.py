1aa93bfc657ac3b5504772852583470ab28ee353
BabariyaMit8702 <mit944996@gmail.com>
2025-07-26 17:23:16 +0530

    add css and js folders in static/app/ folder

--- ecw/urls.py
+++ ecw/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for ecw project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -34,5 +18,6 @@ urlpatterns = [
     path('thnx/',views.thnx,name="thankU"),
     path('img/',views.bs),
     path('cbv/',views.rd.as_view()),
+    path('cj/',views.Imit.as_view())
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
