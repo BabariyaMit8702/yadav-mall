bd73543b27c4f8ad4d67a81144b6163023614c32
BabariyaMit8702 <mit944996@gmail.com>
2025-09-12 10:14:46 +0530

    switch sql engine mysql to postgresql

--- ecw/settings.py
+++ ecw/settings.py
@@ -70,19 +70,31 @@ WSGI_APPLICATION = 'ecw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ecom_project',
+#         'USER': 'yadav',
+#         'PASSWORD': 'parth.priyanshu',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecom_project',
-        'USER': 'yadav',
-        'PASSWORD': 'parth.priyanshu',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'the_ecom',           
+        'USER': 'yadav',         
+        'PASSWORD': 'parth.priyanshu', 
+        'HOST': '127.0.0.1',        
+        'PORT': '5432',            
     }
 }
+
 
 
 
